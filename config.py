f65506ec2f95db88fa7825495736e91d75a9cf7b
KSBOTSOFFICAL <108800837+KSBOTSOFFICAL@users.noreply.github.com>
2023-06-23 14:42:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 sudo_users = [959184369]
 
-iptv_link = "https://gist.githubusercontent.com/KSBOTSOFFICAL/ca5c0028c9d3beb6b48f4a524fc9c6e3/raw/83f159e69a78bbfc1e8a36ecd819a96d4efe6e6e/Bvshbvr.json"
+iptv_link = "https://gist.githubusercontent.com/KSBOTSOFFICAL/5f46c704f6d854bb17e663d782d07853/raw/4be8f4053a5a5a218141537e2d8f8133fc142ace/ylbbinks.json"
 
 GROUP_TAG = "Epic"
 
